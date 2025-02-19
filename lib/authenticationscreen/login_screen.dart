9a6e9f7580fc25ea39de7a83b53b70635d1b4261
skymid <skymidrevengers@gmail.com>
2025-02-19 19:04:59 +0530

    Added custom text field widget Added loading screen

--- lib/authenticationscreen/login_screen.dart
+++ lib/authenticationscreen/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:kuda/widgets/custom_text_field_widget.dart';
 
 class loginScreen extends StatefulWidget {
   const loginScreen({super.key});
@@ -8,6 +9,11 @@ class loginScreen extends StatefulWidget {
 }
 
 class _loginScreenState extends State<loginScreen> {
+
+  TextEditingController emailTextEditingController = TextEditingController();
+  TextEditingController passwordTextEditingController = TextEditingController();
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,6 +37,92 @@ class _loginScreenState extends State<loginScreen> {
                     fontSize: 30,
               ),
               ),
+
+              const SizedBox(
+                height: 60,
+              ),
+
+              //email
+              SizedBox(
+                width: MediaQuery.of(context).size.width - 40,
+                height: 70,
+                child: CustomTextFieldWidget(
+                  editingController: emailTextEditingController,
+                  labelText: "Email",
+                  iconData: Icons.email_outlined,
+                  isObscure: false,
+                ),
+              ),
+
+              //password
+              SizedBox(
+                width: MediaQuery.of(context).size.width - 40,
+                height: 70,
+                child: CustomTextFieldWidget(
+                  editingController: passwordTextEditingController,
+                  labelText: "Password",
+                  iconData: Icons.lock_outlined,
+                  isObscure: true,
+                ),
+              ),
+
+              //login button
+              Container(
+                width: MediaQuery.of(context).size.width - 40,
+                height: 55,
+                decoration: const BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(12),
+                  )
+                ),
+                child: InkWell(
+                  onTap: (){
+
+                  },
+                  child: const Center(
+                    child: Text(
+                      "Login",
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                    ),
+                    )
+                  )
+                ),
+              ),
+
+              const SizedBox(
+                height: 16,
+              ),
+
+              Container(
+                width: MediaQuery.of(context).size.width - 40,
+                height: 55,
+                decoration: const BoxDecoration(
+                    color: Colors.grey,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(12),
+                    )
+                ),
+                child: InkWell(
+                    onTap: (){
+
+                    },
+                    child: const Center(
+                        child: Text(
+                          "Create new account",
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        )
+                    )
+                ),
+              ),
+
             ],
           ),
         ),
